960261898eef09438b762fba42e29e80760290fe
Abhinav Tushar <abhinav@lepisma.xyz>
2020-05-14 22:59:38 +0530

    load experiments in cli

--- abcd/app.py
+++ abcd/app.py
@@ -2,14 +2,20 @@
 abcd allocation server
 
 Usage:
-  abcd [--port=<port>]
+  abcd <experiments-dir> [--port=<port>]
+
+Arguments:
+  <experiments-dir>    Directory to keep experiment files in. This directory
+                       has sub-directories specifying namepace and compiled
+                       planout files.
 
 Options:
-  --port=<port>      Port for server [default: 8813]
+  --port=<port>        Port for server [default: 8813]
 """
 
 from docopt import docopt
 
+from abcd.experiment import load_experiments_from_dir
 from flask import Flask
 
 APP = Flask(__name__)
@@ -24,4 +30,6 @@ def main():
     args = docopt(__doc__)
 
     port = int(args["--port"])
+    experiments = load_experiments_from_dir(args["<experiments-dir>"])
+
     APP.run(port=port)
